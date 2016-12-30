f54e7f2679cd8edc8ac13e5778cc79d0714266a5
Radium Zheng <ztl8702@msn.com>
2016-12-30 20:49:16 +1100

    OCR attempt2

--- processing/OCR.ps1
+++ processing/OCR.ps1
@@ -1,3 +1,7 @@
+chcp 65001;
+$OutputEncoding = New-Object -typename System.Text.UTF8Encoding;
+[Console]::OutputEncoding = $OutputEncoding;
+
 function DoOCR([int]$RangeStart, [int]$RangeEnd) {
     $tabName = "ENGOCR";
 
@@ -11,38 +15,60 @@ function DoOCR([int]$RangeStart, [int]$RangeEnd) {
     $table.columns.add($col1);
     $table.columns.add($col2);
 
-    $files = ls ./;
+    $files = ls ./segments_eng;
     $count = 0;
-    Write-Host ($count);
 
     foreach ($file in $files[$RangeStart..$RangeEnd]) {
+        $count = $count +1;
         #Create a row
         $row = $table.NewRow();
         #Enter data in the row
         $row.FileName = $file.Name; 
-        Write-Host ("[$RangeStart, $RangeEnd]Processing " + $file.Name);
-        $ocrCmd = "tesseract " + $file.Name +" stdout -psm 6 -l eng";
+        Write-Host ("[$RangeStart, $RangeEnd] ($count of $($RangeEnd-$RangeStart+1)) " + $file.Name);
+        # Clean image
+        $tmpFile = "./tmp/tmp_"+$RangeStart+"_"+$RangeEnd+".png";
+        $cleanCmd = "./textcleaner -u segments_eng/" + $file.Name + " " + $tmpFile;
+        bash -c $cleanCmd;
+        # Do OCR
+        $ocrCmd = "tesseract -psm 6 " + $tmpFile +" stdout ./tesseract.conf";
         $result = bash -c $ocrCmd;
         Write-Host($result );
         $row.Text = $result -join ' ';
         #Add the row to the table
         $table.Rows.Add($row);
-        $count = $count+1;
-    # if ($count -gt 8) {
-        #    break;
-    # }
     }
 
     #Display the table
     $table | format-table -AutoSize 
 
     #NOTE: Now you can also export this table to a CSV file as shown below.
-    $filename =  "../EnglishText_" + $RangeStart+"_"+ $RangeEnd+ ".csv";
-    $tabCsv = $table | export-csv $filename -noType -Encoding ASCII;
+    $filename =  "./EnglishText_" + $RangeStart+"_"+ $RangeEnd+ ".csv";
+    $tabCsv = $table | export-csv $filename -noType -Encoding UTF8;
 }
 
 
 function GetRange() {
-    $files = ls ./;
-    Write-Host "0.." + ($files.Length-1);
+    $files = ls ./segments_eng;
+    return 0, ($files.Length-1);
+}
+
+# start powershell "& {Echo $a; Read-Host;}"
+
+function RunJobs() {
+    $ranges = GetRange;
+    $size = $ranges[1] - $ranges[0] +1; 
+    $workers = 10;
+    $eachSize = [math]::floor( $size / $workers);
+    $lastSize = $size - $eachSize * $workers;
+    for ($i = 0; $i -lt $workers; $i++) {
+        $start = $ranges[0] + $i*$eachSize;
+        $end = $start+$eachSize -1;
+        start powershell "& {. ./OCR.ps1; DoOCR $start $end;}";
+        
+    }
+    if ($lastSize -gt 0) {
+        $start = $ranges[0]+$eachSize*$workers;
+        $end = $ranges[1];
+        start powershell "& {. ./OCR.ps1; DoOCR $start $end;}";
+    }
 }
